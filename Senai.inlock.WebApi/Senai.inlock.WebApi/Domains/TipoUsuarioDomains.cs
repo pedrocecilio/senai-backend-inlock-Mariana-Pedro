0ae7b8ace47657f242996905655b7d4287cefef6
pedrocecilio <54940561+pedrocecilio@users.noreply.github.com>
2020-03-03 00:36:36 -0300

    Finalização do Crud

--- Senai.inlock.WebApi/Senai.inlock.WebApi/Domains/TipoUsuarioDomains.cs
+++ Senai.inlock.WebApi/Senai.inlock.WebApi/Domains/TipoUsuarioDomains.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Senai.inlock.WebApi.Domains
 {
     public class TipoUsuarioDomains
     {
+        public int Id_TipoUsuario { get; set; }
+
+        // Define que o título do tipo de usuário é obrigatório
+        [Required(ErrorMessage = "O título do tipo de usuário é obrigatório!")]
+        public string Titulo { get; set; }
+
     }
 }
--- Senai.inlock.WebApi/Senai.inlock.WebApi/Interfaces/ITipoUsuarioRepositorycs.cs
+++ Senai.inlock.WebApi/Senai.inlock.WebApi/Interfaces/ITipoUsuarioRepositorycs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Senai.inlock.WebApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,36 @@ namespace Senai.inlock.WebApi.Interfaces
 {
     interface ITipoUsuarioRepositorycs
     {
+        /// <summary>
+        /// Lista todos os tipos de usuário
+        /// </summary>
+        /// <returns>Retorna uma lista de tipos de usuário</returns>
+        List<TipoUsuarioDomains> Listar();
+
+        /// <summary>
+        /// Busca um tipo de usuário através do ID
+        /// </summary>
+        /// <param name="id">ID do tipo de usuário que será buscado</param>
+        /// <returns>Retorna um tipo de usuário buscado</returns>
+        TipoUsuarioDomains BuscarPorId(int id);
+
+        /// <summary>
+        /// Cadastra um novo tipo de usuário
+        /// </summary>
+        /// <param name="novoTipoUsuario">Objeto novoTipoUsuario que será cadastrado</param>
+        void Cadastrar(TipoUsuarioDomains novoTipoUsuario);
+
+        /// <summary>
+        /// Atualiza um tipo de usuário existente
+        /// </summary>
+        /// <param name="id">ID do tipo de usuário que será alterado</param>
+        /// <param name="TipoUsuarioAtualizado">Objeto TipoUsuarioAtualizado que será alterado</param>
+        void Atualizar(int id, TipoUsuarioDomains TipoUsuarioAtualizado);
+
+        /// <summary>
+        /// Deleta um tipo de usuário existente
+        /// </summary>
+        /// <param name="id">ID do tipo de usuário que será deletado</param>
+        void Deletar(int id);
     }
 }
